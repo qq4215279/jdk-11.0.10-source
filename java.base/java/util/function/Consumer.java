2439fe99559991fdea0b349906379c16a287c5ef
qq4215279 <liu865488158@qq.com>
2023-07-11 14:53:40 +0800

    反射 going

--- java.base/java/util/function/Consumer.java
+++ java.base/java/util/function/Consumer.java
@@ -1,67 +1,48 @@
 /*
  * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
 package java.util.function;
 
 import java.util.Objects;
 
 /**
- * Represents an operation that accepts a single input argument and returns no
- * result. Unlike most other functional interfaces, {@code Consumer} is expected
- * to operate via side-effects.
  *
- * <p>This is a <a href="package-summary.html">functional interface</a>
- * whose functional method is {@link #accept(Object)}.
- *
- * @param <T> the type of the input to the operation
- *
- * @since 1.8
+ * @date 2023/7/11 10:57
  */
 @FunctionalInterface
 public interface Consumer<T> {
 
     /**
-     * Performs this operation on the given argument.
-     *
-     * @param t the input argument
+     * 消费
+     * @date 2023/7/11 10:57
+     * @param t
+     * @return void
      */
     void accept(T t);
 
     /**
-     * Returns a composed {@code Consumer} that performs, in sequence, this
-     * operation followed by the {@code after} operation. If performing either
-     * operation throws an exception, it is relayed to the caller of the
-     * composed operation.  If performing this operation throws an exception,
-     * the {@code after} operation will not be performed.
      *
-     * @param after the operation to perform after this operation
-     * @return a composed {@code Consumer} that performs in sequence this
-     * operation followed by the {@code after} operation
-     * @throws NullPointerException if {@code after} is null
+     * @date 2023/7/11 10:57
+     * @param after
+     * @return java.util.function.Consumer<T>
+     * 问：accept(t);after.accept(t); 明明是没有返回值的，但是前面为什么可以加return呢？？
+     * 答：后来发现return返回的是一个构造Consumer对象的过程
      */
     default Consumer<T> andThen(Consumer<? super T> after) {
         Objects.requireNonNull(after);
-        return (T t) -> { accept(t); after.accept(t); };
+        // 等同于如下源码写法
+       /* return new Consumer<>() {
+            @Override
+            public void accept(T t) {
+                accept(t);
+                after.accept(t);
+            }
+        };*/
+
+        return (T t) -> {
+            accept(t);
+            after.accept(t);
+        };
     }
 }
